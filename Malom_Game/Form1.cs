5a7039de2a05e5f6e33223149194cd41efd4bb45
beretkag <beretkag@gmail.com>
2022-11-02 10:03:01 +0100

    Map Generate

--- Malom_Game/Form1.cs
+++ Malom_Game/Form1.cs
@@ -12,10 +12,48 @@ namespace Malom_Game
 {
     public partial class Form1 : Form
     {
+        Mezo[,,] Palya = new Mezo[3, 3, 3]; // sor, oszlop, z_index
         public Form1()
         {
             InitializeComponent();
-            List<Panel> panelek = new List<Panel>();
+            GeneratePalya();
+
+        }
+
+        private void GeneratePalya()
+        {
+            int nullpozX = 173;
+            int nullpozY = 13;
+            int gap = 97;
+
+
+
+            for (int sor = 0; sor < Palya.GetLength(0); sor++)
+            {
+                for (int oszlop = 0; oszlop < Palya.GetLength(1); oszlop++)
+                {
+                    for (int z_index = 0; z_index < Palya.GetLength(2); z_index++)
+                    {
+                        if (!(oszlop == 1 && sor == 1))
+                        {
+                            PictureBox kep = new PictureBox();
+                            int xHelyzet = nullpozX + z_index * gap + (3 - z_index) * gap * oszlop;
+                            int yHelyzet = nullpozY + z_index * gap + (3 - z_index) * gap * sor;
+                            kep.Location = new Point(xHelyzet, yHelyzet);
+                            kep.Size = new Size(35, 35);
+                            kep.Name = $"kep_{sor}{oszlop}{z_index}";
+                            this.Controls.Add(kep);
+                            Palya[sor, oszlop, z_index] = new Mezo(kep);
+                        } 
+                        else
+                        {
+                            Palya[sor, oszlop, z_index] = null;
+                        }
+                    }
+                }
+            }
+
+
         }
     }
 }
---
+++ Malom_Game/Mezo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Malom_Game
+{
+    class Mezo
+    {
+        public PictureBox Kep;
+        public bool Szabad;
+        public string JatekosNev;
+
+
+
+        public Mezo(PictureBox kep)
+        {
+            Kep = kep;
+            Szabad = true;
+            JatekosNev = null;
+        }
+
+    }
+}

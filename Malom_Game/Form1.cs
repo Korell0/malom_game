b713cf921fd11bf28c8025922515bb888167369a
beretkag <beretkag@gmail.com>
2022-11-07 10:34:47 +0100

    mozgatas függvény meghívása

--- Malom_Game/Form1.cs
+++ Malom_Game/Form1.cs
@@ -12,11 +12,13 @@ namespace Malom_Game
 {
     public partial class Form1 : Form
     {
+        static int korongSize = 35;
         static PictureBox Aktiv = new PictureBox();
         static int korongszam = 9; 
         static Mezo[,,] Palya = new Mezo[3, 3, 3]; // sor, oszlop, z_index
         Jatekos Player1;
         Jatekos Player2;
+        Jatekos ActualPlayer = Player1;
         public Form1(List<string> nevek)
         {
             InitializeComponent();
@@ -30,13 +32,15 @@ namespace Malom_Game
         {
             List<Jatekos> players = new List<Jatekos>() { Player1, Player2 };
             List<GroupBox> boxok = new List<GroupBox>() { groupBox1, groupBox2 };
+            Player1 = new Jatekos(nevek[0], 0, boxok[0]);
+            GenerateKorongok(Player1, boxok[0]);
+            Player2 = new Jatekos(nevek[1], 1, boxok[1]);
+            GenerateKorongok(Player2, boxok[1]);
+
             for (int i = 0; i < players.Count; i++)
             {
-                players[i] = new Jatekos(nevek[i], i);
-                boxok[i].Text = nevek[i];
-                GenerateKorongok(players[i], boxok[i]);
+                boxok[i].Text = nevek[i];                
             }
-
         }
 
         private void GenerateKorongok(Jatekos jatekos, GroupBox groupbox)
@@ -44,7 +48,7 @@ namespace Malom_Game
             for (int i = 0; i < korongszam; i++)
             {
                 PictureBox kep = new PictureBox();
-                kep.Size = new Size(35, 35);
+                kep.Size = new Size(korongSize, korongSize);
                 int gap = 10; // px
                 int xHelyzet = groupbox.Location.X + groupbox.Size.Width / 2 - kep.Size.Width / 2;
                 int yHelyzet = groupbox.Location.Y + groupbox.Size.Height / 2 - (korongszam - 1) * gap / 2 - korongszam * kep.Size.Height / 2 + i*(gap + kep.Size.Height);
@@ -97,7 +101,7 @@ namespace Malom_Game
                             int xHelyzet = nullpozX + z_index * gap + (3 - z_index) * gap * oszlop;
                             int yHelyzet = nullpozY + z_index * gap + (3 - z_index) * gap * sor;
                             kep.Location = new Point(xHelyzet, yHelyzet);
-                            kep.Size = new Size(35, 35);
+                            kep.Size = new Size(korongSize, korongSize);
                             kep.Name = $"kep_{sor}{oszlop}{z_index}";
                             kep.SizeMode = PictureBoxSizeMode.Zoom;
                             kep.BackColor = Color.Transparent;
@@ -121,7 +125,7 @@ namespace Malom_Game
         {
             Rectangle r = new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height);
             System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
-            int d = 35;
+            int d = korongSize;
             gp.AddArc(r.X, r.Y, d, d, 180, 90);
             gp.AddArc(r.X + r.Width - d, r.Y, d, d, 270, 90);
             gp.AddArc(r.X + r.Width - d, r.Y + r.Height - d, d, d, 0, 90);
@@ -137,10 +141,30 @@ namespace Malom_Game
                 {
                     kep.Image = Aktiv.Image;
                     this.Controls.Remove(Aktiv);
+                    ActualPlayer.JatszoKorongok.Add(Aktiv);
+                    ActualPlayer.KezdoKorongok.Remove(Aktiv);
+                    if (ActualPlayer.KezdoKorongok.Count < 1)
+                    {
+                        ActualPlayer.Lephet = true;
+                    }
+                    ActualPlayer = new List<Jatekos>() { Player1, Player2 }.Find(x => x != ActualPlayer);
                 }
                 Aktiv = null;
             }
+            else if (ActualPlayer.Lephet && ActualPlayer.JatszoKorongok.Contains(kep))
+            {
+                Mozgat(kep);
+            }
+
             Normalize();
+            MessageBox.Show(Player1.KezdoKorongok[0].Name);
+        }
+
+        private void Mozgat(PictureBox kep)
+        {
+            
+
+
 
         }
 
--- Malom_Game/Jatekos.cs
+++ Malom_Game/Jatekos.cs
@@ -13,12 +13,18 @@ namespace Malom_Game
         public string Nev;
         public int KorongJel;
         public List<PictureBox> KezdoKorongok;
+        public GroupBox Groupbox;
+        public bool Lephet;
+        public List<PictureBox> JatszoKorongok;
 
-        public Jatekos(string nev, int jel)
+        public Jatekos(string nev, int jel, GroupBox groupbox)
         {
             Nev = nev;
             KorongJel = jel;
             KezdoKorongok = new List<PictureBox>();
+            Groupbox = groupbox;
+            Lephet = false;
+            JatszoKorongok = new List<PictureBox>();
         }
 
 
